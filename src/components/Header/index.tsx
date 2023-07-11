ce74955b732e5164b09a2aa3689f2fef957ec93f
Caio Ribeiro de Carvalho <caiocarvalho314@gmail.com>
2023-07-11 09:23:03 -0300

    start repository

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import {HeaderContainer} from "./styles";
+
+export default function Header() {
+  return (
+    <>
+      <h1>Header</h1>
+    </>
+  );
+}
---
+++ src/pages/History.tsx
@@ -0,0 +1,7 @@
+export function History() {
+  return (
+    <>
+      <h1>Página de History</h1>
+    </>
+  );
+}
